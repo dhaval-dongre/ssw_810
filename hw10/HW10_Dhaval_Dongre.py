04fe7a97bd3b18815d0f5c73f1bc9cc6d43faa30
dhaval-dongre <dhavaldongre@gmail.com>
2019-11-07 20:11:52 -0500

    added missing files

--- hw10/HW10_Dhaval_Dongre.py
+++ hw10/HW10_Dhaval_Dongre.py
@@ -11,7 +11,6 @@ class Repository:
         self.directory = directory
         self.student = self.Student(directory)
         self.instructor = self.Instructor(directory)
-                
 
     class Student:
         """contains info for students and stores information regarding their id's, courses and grades"""
@@ -19,14 +18,12 @@ class Repository:
         def __init__(self, directory):
             self.gradesList = defaultdict(str)
             self.summary = defaultdict()
-            self.core_summary=defaultdict()
-            self.elective_summary=defaultdict()
-            self.directory=directory
-            
+            self.core_summary = defaultdict()
+            self.elective_summary = defaultdict()
+            self.directory = directory
 
-            self.core_summary=defaultdict()
-            self.elective_summary=defaultdict()
-            count=0
+            self.core_summary = defaultdict()
+            self.elective_summary = defaultdict()
 
             for l in file_reading_gen(
                     os.path.join(
@@ -40,9 +37,7 @@ class Repository:
                     self.core_summary[l[0]] = list()
                     self.elective_summary[l[0]] = list()
 
-                # count+=1
-
-                if l[1]=='R':
+                if l[1] == 'R':
                     self.core_summary[l[0]].append(l[2])
                 else:
                     self.elective_summary[l[0]].append(l[2])
@@ -53,7 +48,7 @@ class Repository:
                         directory,
                         'grades.txt'),
                     4,
-                    '|',True))
+                    '|', True))
 
             for l in file_reading_gen(
                     os.path.join(
@@ -63,19 +58,40 @@ class Repository:
                     ';', True):
                 self.summary[l[0]] = list()
                 self.summary[l[0]].append(l[1])
+                self.summary[l[0]].append(l[2])
                 courseList = list()
+
                 for g in gList:
                     if l[0] == g[0]:
                         self.gradesList[g[1]] = g[2]
                         courseList.append(g[1])
                 self.summary[l[0]].extend(sorted(courseList))
 
+        def pretty_printMajor(self):
+            y = PrettyTable()
+            y.field_names = [
+                "Dept",
+                "Required",
+                "Electives"
+            ]
+
+            for k1, v1 in self.core_summary.items():
+                k = list()
+                k.append(k1)
+                k.append(list(v1))
+                k.append(self.elective_summary[k1])
+                y.add_row(k)
+
+            print('\n')
+            print(y)
+
         def pretty_print(self):
-            """ prints the cwid, name and courses for each student"""
+            """ prints the cwid, name and courses for each student and also prints the major and its electives and required subjects"""
             x = PrettyTable()
             x.field_names = [
                 "CWID",
                 "Name",
+                "Major",
                 "Completed Courses",
                 "Remaining Required",
                 "Remaining Electives"
@@ -95,36 +111,40 @@ class Repository:
                 l = list()
                 l.append(k1)
                 l.append(v1[0])
-                l.append(v1[1:])
+                l.append(v1[1])
+                l.append(v1[2:])
 
-                yo1=self.core_summary[v1[1]]
-                yo2=list(v1[2:])
-                core=self.core_summary[v1[1]]-list(v1[2:])
+                core = list()
+                elective = list()
+                l1 = list(self.core_summary.values())
+                l2 = [item for sublist in l1 for item in sublist]
+                for course in v1[2:]:
+                    if course in l2:
+                        core.append(course)
+                    else:
+                        elective.append(course)
 
                 for c in core:
 
                     for g in gList:
-                        if g[1]==c and g[0]==k1:
+                        if g[1] == c and g[0] == k1:
 
                             if g[2] not in passing_grades:
                                 core.remove(c)
                                 break
 
-                l.append(core)
-
-                elective=self.elective_summary[v1[1]]-list(v1[2:])
+                l.append(None if not core else set(core))
 
                 for c in elective:
 
                     for g in gList:
-                        if g[1]==c and g[0]==k1:
+                        if g[1] == c and g[0] == k1:
 
                             if g[2] not in passing_grades:
                                 elective.remove(c)
                                 break
 
-                        
-                l.append(elective)
+                l.append(None if not elective else set(elective))
                 x.add_row(l)
 
             print('\n')
@@ -143,14 +163,14 @@ class Repository:
                         directory,
                         'grades.txt'),
                     4,
-                    '|',True))
+                    '|', True))
 
             for l in file_reading_gen(
                     os.path.join(
                         directory,
                         'instructors.txt'),
                     3,
-                    '|',True):
+                    '|', True):
 
                 for g in gList:
                     if l[0] == g[3]:
@@ -188,3 +208,4 @@ if __name__ == '__main__':
     r = Repository('C:/Users/Dhaval/Documents/ssw_810/hw10/data/')
     r.student.pretty_print()
     r.instructor.pretty_print()
+    r.student.pretty_printMajor()
